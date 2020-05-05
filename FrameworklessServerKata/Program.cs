ff9bad04980a9261d0f2d6f2536e6eb3aa9621ec
davetingbro <davidting3250@gmail.com>
2020-05-05 15:40:17 +1200

    first commit

---
+++ FrameworklessServerKata/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+
+namespace FrameworklessServerKata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var server = new HttpListener();
+            server.Prefixes.Add("http://localhost:8080/");
+            server.Start();
+            while (true)
+            {
+                var context = server.GetContext();  // Gets the request
+                Console.WriteLine($"{context.Request.HttpMethod} {context.Request.Url}");
+                var buffer = System.Text.Encoding.UTF8.GetBytes("Hello David - the time on the server is ");
+                context.Response.ContentLength64 = buffer.Length;
+                context.Response.OutputStream.Write(buffer, 0, buffer.Length);  // forces send of response
+            }
+            server.Stop();  // never reached...
+        }
+    }
+}
