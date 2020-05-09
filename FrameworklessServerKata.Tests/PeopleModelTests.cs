c493317cd971f2981690bf4893e61b43cb6e8bee
davetingbro <davidting3250@gmail.com>
2020-05-10 11:54:03 +1200

    can delete person from People list

--- FrameworklessServerKata.Tests/PeopleModelTests.cs
+++ FrameworklessServerKata.Tests/PeopleModelTests.cs
@@ -7,12 +7,17 @@ namespace FrameworklessServerKata.Tests
 {
     public class PeopleModelTests
     {
+        private readonly PeopleModel _peopleModel;
+
+        public PeopleModelTests()
+        {
+            _peopleModel = new PeopleModel();
+        }
+
         [Fact]
         public void ShouldHavePersonNamedDavidByDefault_WhenInstantiated()
         {
-            var peopleModel = new PeopleModel();
-
-            var result = peopleModel.People;
+            var result = _peopleModel.People;
 
             var resultJson = JsonConvert.SerializeObject(result);
             var expectedJson = JsonConvert.SerializeObject(new List<Person> {new Person("David")});
@@ -22,16 +27,31 @@ namespace FrameworklessServerKata.Tests
         [Fact]
         public void ShouldAddGivenPersonToPeopleList()
         {
-            var peopleModel = new PeopleModel();
-
-            peopleModel.Add(new Person("Michael"));
-            var result = peopleModel.People;
+            _peopleModel.Add(new Person("Michael"));
+            var result = _peopleModel.People;
             var expected = new List<Person>
             {
                 new Person("David"), new Person("Michael")
             };
 
             var resultJson = JsonConvert.SerializeObject(result);
+            var expectedJson = JsonConvert.SerializeObject(expected);
+            Assert.Equal(expectedJson, resultJson);
+        }
+        
+        [Fact]
+        public void ShouldDeletePersonFromList_GivenName()
+        {
+            _peopleModel.Add(new Person("Michael"));
+            _peopleModel.Add(new Person("Will"));
+
+            _peopleModel.Delete("Will");
+            var expected = new List<Person>
+            {
+                new Person("David"), new Person("Michael")
+            };
+
+            var resultJson = JsonConvert.SerializeObject(_peopleModel.People);
             var expectedJson = JsonConvert.SerializeObject(expected);
             Assert.Equal(expectedJson, resultJson);
         }
--- FrameworklessServerKata/PeopleModel.cs
+++ FrameworklessServerKata/PeopleModel.cs
@@ -4,7 +4,7 @@ namespace FrameworklessServerKata
 {
     public class PeopleModel
     {
-        public List<Person> People { get; set; }
+        public List<Person> People { get; }
 
         public PeopleModel()
         {
@@ -14,6 +14,11 @@ namespace FrameworklessServerKata
         public void Add(Person person)
         {
             People.Add(person);
+        }
+
+        public void Delete(string name)
+        {
+            People.RemoveAll(p => p.Name == name);
         }
     }
 }
