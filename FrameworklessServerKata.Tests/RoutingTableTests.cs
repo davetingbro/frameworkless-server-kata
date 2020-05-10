44d7961442233ae98915a251518c1b141a73f00e
davetingbro <davidting3250@gmail.com>
2020-05-11 09:07:26 +1200

    unit test - can return PersonRequestHandler

--- FrameworklessServerKata.Tests/RoutingTableTests.cs
+++ FrameworklessServerKata.Tests/RoutingTableTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using FrameworklessServerKata.RequestControllers;
 using Xunit;
 
@@ -6,15 +7,22 @@ namespace FrameworklessServerKata.Tests
 {
     public class RoutingTableTests
     {
+        private readonly RoutingTable _routingTable;
+        private readonly List<Person> _people;
+
+        public RoutingTableTests()
+        {
+            _routingTable = new RoutingTable();
+            _people = new List<Person> {new Person("David"), new Person("Michael"), new Person("Will")};
+        }
+        
         [Theory]
         [InlineData("http://localhost:8080/", typeof(GreetRequestController))]
         [InlineData("http://localhost:8080/people", typeof(PeopleRequestController))]
-
+        [InlineData("http://localhost:8080/people/david", typeof(PersonRequestController))]
         public void ShouldReturnCorrectRequestController_WhenGivenValidUrl(string url, Type expectedType)
         {
-            var routingTable = new RoutingTable();
-
-            var result = routingTable.GetRequestController(url);
+            var result = _routingTable.GetRequestController(url, _people);
 
             Assert.IsType(expectedType, result);
         }
--- FrameworklessServerKata/RoutingTable.cs
+++ FrameworklessServerKata/RoutingTable.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 using FrameworklessServerKata.Interface;
 using FrameworklessServerKata.RequestControllers;
 
@@ -6,14 +9,25 @@ namespace FrameworklessServerKata
 {
     public class RoutingTable
     {
-        public IRequestController GetRequestController(string url)
+        public IRequestController GetRequestController(string url, List<Person> people)
         {
             return url switch
             {
                 "http://localhost:8080/" => new GreetRequestController(),
                 "http://localhost:8080/people" => new PeopleRequestController(),
+                _ when Regex.IsMatch(url, @"http://localhost:8080/people/\w+") 
+                        => GetPersonRequestController(url, people),
                 _ => throw new ArgumentException("url not found")
             };
+        }
+
+        private IRequestController GetPersonRequestController(string url, List<Person> people)
+        {
+            var personUrls = people.Select(p 
+                => $"http://localhost:8080/people/{p.Name.ToLower()}");
+            return personUrls.Contains(url)
+                ? new PersonRequestController()
+                : throw new ArgumentException("url not found");
         }
     }
 }
