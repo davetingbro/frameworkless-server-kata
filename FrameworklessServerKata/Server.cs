e98f17483d8a5addad70876324695a6c6c0e1d8d
davetingbro <davidting3250@gmail.com>
2020-05-14 09:42:30 +1200

    implement server

--- FrameworklessServerKata/Server.cs
+++ FrameworklessServerKata/Server.cs
@@ -1,15 +1,18 @@
 using System;
+using System.IO;
 using System.Net;
+using FrameworklessServerKata.RequestControllers;
 
 namespace FrameworklessServerKata
 {
     public class Server
     {
-        private readonly HttpListener _listener;
+        private readonly HttpListener _listener = new HttpListener();
+        private readonly RoutingTable _routingTable = new RoutingTable();
+        private readonly PeopleModel _peopleModel = new PeopleModel();
 
         public Server()
         {
-            _listener = new HttpListener();
             _listener.Prefixes.Add("http://localhost:8080/");
         }
 
@@ -19,14 +22,49 @@ namespace FrameworklessServerKata
             while (true)
             {
                 var context = _listener.GetContext();
-                Console.WriteLine($"{context.Request.HttpMethod} {context.Request.Url}");
-                var time = DateTime.Now.ToShortTimeString();
-                var date = DateTime.Now.ToLongDateString();
-                var buffer = System.Text.Encoding.UTF8.GetBytes(
-                    $"Hello David - the time on the server is {time} on {date}");
-                context.Response.ContentLength64 = buffer.Length;
-                context.Response.OutputStream.Write(buffer, 0, buffer.Length);
+                var httpRequest = context.Request;
+
+                Console.WriteLine($"{httpRequest.HttpMethod} {httpRequest.Url}");
+                var response = CreateResponse(httpRequest);
+                SetHttpListenerResponse(context, response);
             }
+        }
+
+        private Response CreateResponse(HttpListenerRequest request)
+        {
+            try
+            {
+                var controller = _routingTable.GetRequestController(request.Url.AbsoluteUri, _peopleModel);
+                return ExecuteRequestMethod(request, controller);
+            }
+            catch (ArgumentException)
+            {
+                return new Response(404);
+            }
+        }
+
+        private static Response ExecuteRequestMethod(HttpListenerRequest request, RequestController controller)
+        {
+            var requestMethod = $"{request.HttpMethod}";
+            var body = new StreamReader(request.InputStream).ReadToEnd();
+            return requestMethod switch
+            {
+                "GET" => controller.Get(),
+                "POST" => controller.Post(body),
+                "PUT" => controller.Put(),
+                "DELETE" => controller.Delete(),
+                _ => new Response(404)
+            };
+        }
+
+        private static void SetHttpListenerResponse(HttpListenerContext context, Response response)
+        {
+            var buffer = System.Text.Encoding.UTF8.GetBytes(response.Body);
+            var httpResponse = context.Response;
+            httpResponse.StatusCode = response.StatusCode;
+            httpResponse.ContentLength64 = buffer.Length;
+            httpResponse.OutputStream.Write(buffer, 0, buffer.Length);
+            httpResponse.OutputStream.Close();
         }
 
     }
