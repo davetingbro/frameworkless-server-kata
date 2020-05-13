e3e0f42ad581313a040eee1e3cd745fb302b3e13
davetingbro <davidting3250@gmail.com>
2020-05-13 16:35:37 +1200

    refactor - private PeopleModel field

--- FrameworklessServerKata.Tests/CommandTests/GetPersonCommandTests.cs
+++ FrameworklessServerKata.Tests/CommandTests/GetPersonCommandTests.cs
@@ -18,9 +18,9 @@ namespace FrameworklessServerKata.Tests.CommandTests
         [Fact]
         public void ShouldReturnCorrectResponseWhenExecuted()
         {
-            var command = new GetPersonCommand("Michael");
+            var command = new GetPersonCommand(_peopleModel);
 
-            var result = command.Execute(_peopleModel);
+            var result = command.Execute("Michael");
             var expected = new Response("200", "Michael");
 
             var resultJson = JsonConvert.SerializeObject(result);
--- FrameworklessServerKata/Commands/GetPersonCommand.cs
+++ FrameworklessServerKata/Commands/GetPersonCommand.cs
@@ -3,10 +3,22 @@ namespace FrameworklessServerKata.Commands
     public class GetPersonCommand : ICommand
     {
         private readonly string _personName;
+        private readonly PeopleModel _peopleModel;
+
+        public GetPersonCommand(PeopleModel peopleModel)
+        {
+            _peopleModel = peopleModel;
+        }
 
         public GetPersonCommand(string personName)
         {
             _personName = personName;
+        }
+        
+        public Response Execute(string reqParam="", string reqBody = "")
+        {
+            var person = _peopleModel.Find(reqParam);
+            return new Response("200", person.Name);
         }
 
         public Response Execute(PeopleModel peopleModel, string body="")
