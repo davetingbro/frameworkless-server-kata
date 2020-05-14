f7d8843bfe5c2bc44aa57fb01e590811270ddc3b
davetingbro <davidting3250@gmail.com>
2020-05-15 09:41:52 +1200

    program try catch

--- FrameworklessServerKata/Program.cs
+++ FrameworklessServerKata/Program.cs
@@ -1,11 +1,22 @@
-﻿namespace FrameworklessServerKata
+﻿using System;
+
+namespace FrameworklessServerKata
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var server = new Server();
-            server.Start();
+            try
+            {
+                var server = new Server();
+                server.Start();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("You have crashed the server...");
+                Console.WriteLine(e.Message);
+                throw;
+            }
         }
     }
 }
