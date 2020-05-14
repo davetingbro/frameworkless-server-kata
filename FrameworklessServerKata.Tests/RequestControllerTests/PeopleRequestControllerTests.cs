0d8833f4f707d8511a8ed834093f6a4d1dc0f4a3
davetingbro <davidting3250@gmail.com>
2020-05-14 21:17:47 +1200

    Post method unit test

--- FrameworklessServerKata.Tests/RequestControllerTests/PeopleRequestControllerTests.cs
+++ FrameworklessServerKata.Tests/RequestControllerTests/PeopleRequestControllerTests.cs
@@ -1,4 +1,5 @@
-using FrameworklessServerKata.Commands;
+using System.Collections.Generic;
+using System.Linq;
 using FrameworklessServerKata.RequestControllers;
 using Newtonsoft.Json;
 using Xunit;
@@ -27,6 +28,60 @@ namespace FrameworklessServerKata.Tests.RequestControllerTests
             var resultJson = JsonConvert.SerializeObject(result);
             var expectedJson = JsonConvert.SerializeObject(expected);
             Assert.Equal(expectedJson, resultJson);
+        }
+        
+        [Fact]
+        public void PostShouldAddNewPersonToPeople()
+        {
+            var controller = new PeopleRequestController(_peopleModel);
+
+            controller.Post("John");
+            var result = _peopleModel.People.Select(p => p.Name).ToList();
+            var expected = new List<string>{"David", "Michael", "Will", "John"};
+            
+            Assert.Equal(expected, result);
+        }
+        
+        [Fact]
+        public void PostShouldReturnResponseStatusCode201_IfSuccessfullyAdded()
+        {
+            var controller = new PeopleRequestController(_peopleModel);
+
+            var result = controller.Post("John");
+            
+            Assert.Equal(201, result.StatusCode);
+        }
+
+        [Fact]
+        public void PostShouldReturnResponseStatusCode202IfPersonAlreadyExist()
+        {
+            var controller = new PeopleRequestController(_peopleModel);
+
+            var result = controller.Post("David");
+            
+            Assert.Equal(202, result.StatusCode);
+        }
+
+        [Fact]
+        public void PostShouldReturnResponseStatusCode202IfRequestBodyIsEmpty()
+        {
+            var controller = new PeopleRequestController(_peopleModel);
+
+            var result = controller.Post("");
+            
+            Assert.Equal(202, result.StatusCode);
+        }
+
+        [Fact]
+        public void Post_PeopleListShouldRemainUnchangedIfRespondedWith202()
+        {
+            var controller = new PeopleRequestController(_peopleModel);
+            controller.Post("David");
+
+            var result = _peopleModel.People.Select(p => p.Name).ToList();
+            var expected = new List<string>{"David", "Michael", "Will"};
+            
+            Assert.Equal(expected, result);
         }
     }
 }
